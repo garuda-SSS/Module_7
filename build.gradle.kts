3ae1cd9d393f02321c6fcb09ba3e9845602926d3
garuda-SSS <ysipin1997@gmail.com>
2025-07-27 19:14:22 +0300

    Основа проекта

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+    id("io.qameta.allure") version "2.12.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("com.codeborne:selenide:7.2.1")
+    testImplementation("io.github.bonigarcia:webdrivermanager:5.7.0")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+    testImplementation("io.qameta.allure:allure-junit5:2.29.1")
+    testImplementation("io.qameta.allure:allure-assertj:2.29.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
